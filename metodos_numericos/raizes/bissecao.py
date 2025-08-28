55d54659eb2bff1b9b86ef7dd7e299154930d4e6
Bruno Lempp <brunolempp@outlook.com>
2025-08-28 17:37:32 -0300

    refactor: aprimorado a validação de entrada de dados

--- metodos_numericos/raizes/bissecao.py
+++ metodos_numericos/raizes/bissecao.py
@@ -7,7 +7,9 @@ def num_iteracoes(a, b, precisao):
 
 def entrada_funcao():
     try:
-        funcao = input(">Digite a função (em termos de x): ")
+        funcao = input(">Digite a função (em termos de x) ou 'sair' para finalizar: ")
+        if funcao.lower() == "sair":
+            return "sair"
         funcao_numerica = conversor_str_funcao(funcao, 'x')
         return funcao_numerica
     except Exception as e:
@@ -16,16 +18,33 @@ def entrada_funcao():
 
 def entrada_intervalo():
     try:
-        a = float(input(">Digite o limite inferior (a): "))
-        b = float(input(">Digite o limite superior (b): "))
+        a = input(">Digite o limite inferior (a) ou 'sair' para finalizar: ")
+        b = input(">Digite o limite superior (b) ou 'sair' para finalizar: ")
+
+        if a.lower() == "sair" or b.lower() == "sair":
+            return "sair", "sair"
+        
+        a = float(a)
+        b = float(b)
+
+        if a >= b:
+            raise ValueError("O limite inferior deve ser menor que o limite superior.")
         return a, b
     except ValueError:
-        print("Entrada inválida dos limites inválidas.")
+        print(f"Entrada inválida dos limites.: {ValueError}")
         return None
 
 def entrada_precisao():
     try:
-        precisao = float(input(">Digite a precisão (Ex: 1e-5): "))
+        precisao = input(">Digite a precisão (Ex: 1e-5) ou 'sair' para finalizar: ")
+        if precisao.lower() == "sair":
+            return "sair"
+        
+        precisao = float(precisao)
+
+        if precisao <= 0:
+            raise ValueError
+        
         return precisao
     except ValueError:
         print("Entrada inválida.")
@@ -43,16 +62,33 @@ def imprimir_resultado(iteracao, a, b, xn, fa, fxn):
 
 
 def bissecao():
-    funcao = entrada_funcao()
-    a, b = entrada_intervalo()
-    precisao = entrada_precisao()
 
-    if funcao is None or a is None or b is None or precisao is None:
-        print("Entrada inválida.")
+    funcao = entrada_funcao()
+    while funcao != "sair" and funcao is None:
+        print("Função inválida, digite corretamente a função.")
+        funcao = entrada_funcao()
+    if funcao == "sair":
+        print("Finalizando...")
+        return None
+    
+    a, b = entrada_intervalo()
+    while a != "sair" and b != "sair" and (a is None or b is None or funcao(a) * funcao(b) > 0 or a >= b):
+        print("Intervalo inválido. f(a) e f(b) devem ter sinais opostos.")
+        a, b = entrada_intervalo()
+    if a == "sair" or b == "sair":
+        print("Finalizando...")
+        return None
+
+    precisao = entrada_precisao()
+    while precisao != "sair" and precisao is None:
+        print("Precisão inválida. Digite novamente.")
+        precisao = entrada_precisao()
+    if precisao == "sair":
+        print("Finalizando...")
         return None
 
     k_parada = num_iteracoes(a, b, precisao)
-    print(f"Número de iterações necessárias: {k_parada}")
+    print(f"Número de iterações máxima: {k_parada}")
 
     tabela_bissecao()
 
